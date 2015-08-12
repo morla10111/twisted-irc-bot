56b276b5d38d15164b9123154022c2ca563c27c7
mofoe <morla@cracksucht.de>
2015-08-12 13:49:46 +0200

    lol

--- runbot.sh
+++ runbot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./bot.py  --server cracksucht.de \
+./bot.py  --server someserver \
                 --port 6667 \
                 --ssl \
-                --channels '#10111,#news,#beercave'\
-                --pass 'iamtheone'\
+                --channels '#chan'\
+                --pass 'some pass'\
                 --mynick 'royal_tart_toter'\
                 --op morla
 
